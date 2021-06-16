147733888f089203ff29e850d9c493067c9c9336
Jackx201 <61862847+Jackx201@users.noreply.github.com>
2021-06-16 10:31:30 -0500

    Initial commit

---
+++ Assets/Scripts/Count_Score.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Count_Score : MonoBehaviour
+{
+    public Text Scoreboard;
+    public GameObject ball;
+
+    public static bool canAddScore = true;
+
+    private int Bat_1_Score = 0;
+    private int Bat_2_Score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball = GameObject.Find("Ball");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ball.transform.position.x >= 10f && canAddScore)
+        {
+            canAddScore = false;
+            Bat_1_Score++;
+        }
+        if (ball.transform.position.x <= -10f && canAddScore)
+        {
+            canAddScore = false;
+            Bat_2_Score++;
+        }
+
+        if(Bat_1_Score >= 3)
+        {
+            SceneManager.LoadScene(3);
+        }
+        if (Bat_2_Score >= 3)
+        {
+            SceneManager.LoadScene(2);
+        }
+        Scoreboard.text = Bat_1_Score.ToString() + " - " + Bat_2_Score+"";
+        //print(Bat_1_Score + " , " + Bat_2_Score);
+    }
+}
---
+++ Assets/Scripts/Menu_Controller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu_Controller : MonoBehaviour
+{
+    public Button Play_Button;
+    public Button Quit_Button;
+    public void Play_Game()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Quit_Game()
+    {
+        Application.Quit();
+    }
+}
